53d46f97e9994f253ec0e3da1efb9e0fe1ece40b
Stewart ilondanga <stilondanga@gmail.com>
2019-04-18 14:53:21 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,46 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$('#card1').hide();
+let btn = document.getElementById('show');
+btn.addEventListener('click',function(){
+    $('#card1').show();
+    btn.disabled = true;
+  document.getElementById('inner').innerText= "";
+});
+var code = `<div class='topnav'>
+<a class='active' href='#home'>Home</a>
+<a href='#news'>News</a>
+<a href='#contact'>Contact</a>
+<a href='#about'>About</a>
+</div>
+---The Css File---
+.topnav { overflow: hidden;}`;
+document.getElementById('inner').innerText= code;
+
+var ans = document.getElementById('givenAnswer');
+var ansBtn = document.getElementById('answerBtn');
+ans.innerHTML = "";
+ans.placeholder = `<div class='topnav'>
+<a href='#'>Given Value</a>
+</div>`;
+var answer = `<div class='topnav'>
+<a href='#'>About Me</a>
+<a href='#'>Gallery</a>
+<a href='#'>Contact</a>
+</div>`;
+
+ansBtn.addEventListener('click',function(){
+  if(ans.value == ''){
+    alert('Enter a Value');
+  }
+  else{
+    if(ans.value === answer){
+      alert('Good Job');
+    }
+    else{
+      alert('Write all elements and tags and classes properly');
+    }
+  }
+});
